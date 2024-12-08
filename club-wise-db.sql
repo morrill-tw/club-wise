01eee1aa385817ddb651bddcbeec1b5e4dba92b0
oilagh <tdk737@gmail.com>
2024-12-08 05:12:27 -0500

    added procedures

--- club-wise-db.sql
+++ club-wise-db.sql
@@ -19,76 +19,76 @@ CREATE TABLE club
 
 CREATE TABLE role
 (
-	role_name VARCHAR(64) PRIMARY KEY
+    role_name VARCHAR(64) PRIMARY KEY
 );
 
 CREATE TABLE member_of_club
 (
-	member_id INT PRIMARY KEY AUTO_INCREMENT,
+    member_id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(64) NOT NULL,
     last_name VARCHAR(64) NOT NULL,
-	join_date DATE NOT NULL,
+    join_date DATE NOT NULL,
     club_name VARCHAR(64) NOT NULL,
     role_name VARCHAR(64) NOT NULL,
-    
+
     FOREIGN KEY (club_name) REFERENCES club(club_name),
     FOREIGN KEY (role_name) REFERENCES role(role_name)
 );
 
 CREATE TABLE purchase
 (
-	purchase_id INT PRIMARY KEY AUTO_INCREMENT,
+    purchase_id INT PRIMARY KEY AUTO_INCREMENT,
     purhcase_title VARCHAR(64) NOT NULL,
     purchase_description VARCHAR(500),
     cost DECIMAL NOT NULL,
     purchase_date DATE NOT NULL,
     club_name VARCHAR(64) NOT NULL,
-    
+
     FOREIGN KEY (club_name) REFERENCES club(club_name)
 );
 
 CREATE TABLE social_media
 (
-	platform VARCHAR(64) NOT NULL,
+    platform VARCHAR(64) NOT NULL,
     username VARCHAR(64) NOT NULL,
     club_name VARCHAR(64) NOT NULL,
-    
+
     PRIMARY KEY (platform, username),
     FOREIGN KEY (club_name) REFERENCES club(club_name)
 );
 
 CREATE TABLE event
 (
-	event_title VARCHAR(64) NOT NULL,
+    event_title VARCHAR(64) NOT NULL,
     event_description VARCHAR(500) NOT NULL,
     event_date DATE NOT NULL,
     club_name VARCHAR(64) NOT NULL,
-    
+
     PRIMARY KEY (event_title, event_date, club_name),
     FOREIGN KEY (club_name) REFERENCES club(club_name)
 );
 
 CREATE TABLE member_attends_event
 (
-	event_title VARCHAR(64) NOT NULL,
+    event_title VARCHAR(64) NOT NULL,
     event_date DATE NOT NULL,
     club_name VARCHAR(64) NOT NULL,
     member_id INT NOT NULL,
-    
+
     PRIMARY KEY (event_title, event_date, club_name, member_id),
     FOREIGN KEY (event_title, event_date, club_name) REFERENCES event(event_title, event_date, club_name),
     FOREIGN KEY (member_id) REFERENCES member_of_club(member_id)
 );
 
 DELIMITER $$
-CREATE PROCEDURE get_clubs_by_category( 
-IN category_p enum("STEM", 
-				"Arts and Culture", 
-                "Sports and Recreation", 
-                "Community Service and Social Impact", 
-                "Social and Special Interests",
-                "Media and Communications",
-                "Religious and Spiritual") )
+CREATE PROCEDURE get_clubs_by_category(
+    IN category_p enum("STEM",
+    "Arts and Culture",
+    "Sports and Recreation",
+    "Community Service and Social Impact",
+    "Social and Special Interests",
+    "Media and Communications",
+    "Religious and Spiritual") )
 BEGIN
 SELECT * FROM club WHERE category = category_p;
 END $$
@@ -96,24 +96,24 @@ DELIMITER ;
 
 -- For testing
 INSERT INTO club ( club_name, club_description, active, category )
-		VALUES ( "NER", "Make cool electric cars!", TRUE, "STEM" ),
-        ("Cooking Club", "Make food", FALSE, "Social and Special Interests"),
-        ("Generate", "Make prototypes", TRUE, "STEM");
+VALUES ( "NER", "Make cool electric cars!", TRUE, "STEM" ),
+       ("Cooking Club", "Make food", FALSE, "Social and Special Interests"),
+       ("Generate", "Make prototypes", TRUE, "STEM");
 CALL get_clubs_by_category("Social and Special Interests");
 CALL get_clubs_by_category("STEM");
 
 DELIMITER $$
-CREATE PROCEDURE create_club( 
-name_p VARCHAR(64),
-description_p VARCHAR(500),
-active_p BOOL,
-category_p enum("STEM", 
-				"Arts and Culture", 
-                "Sports and Recreation", 
-                "Community Service and Social Impact", 
-                "Social and Special Interests",
-                "Media and Communications",
-                "Religious and Spiritual") )
+CREATE PROCEDURE create_club(
+    name_p VARCHAR(64),
+    description_p VARCHAR(500),
+    active_p BOOL,
+    category_p enum("STEM",
+    "Arts and Culture",
+    "Sports and Recreation",
+    "Community Service and Social Impact",
+    "Social and Special Interests",
+    "Media and Communications",
+    "Religious and Spiritual") )
 BEGIN
 INSERT INTO club ( club_name, club_description, active, category )
 VALUES ( name_p, description_p, active_p, category_p );
@@ -148,27 +148,180 @@ CALL get_members_by_club_name( "NER" );
 
 INSERT INTO role ( role_name )
 VALUES ( "President" ), ( "Member" );
-    
+
 INSERT INTO member_of_club ( first_name, last_name, club_name, role_name, join_date )
 VALUES ("Bob", "Smith", "NER", "President" , '2023-8-10'),
-("Sally", "Parker", "NER", "Member", '2024-8-21'),
-("Bertha", "Dillon", "Cooking Club", "President", '2022-9-1' );
+       ("Sally", "Parker", "NER", "Member", '2024-8-21'),
+       ("Bertha", "Dillon", "Cooking Club", "President", '2022-9-1' );
 
 INSERT INTO member_of_club ( first_name, last_name, club_name, role_name, join_date )
 VALUES ( "Jill", "Brown", "NER", "Member" , '2023-8-10'),
-("Robby", "Lloyd", "NER", "Member", '2024-8-21'),
-("Jackson", "Dumont", "NER", "Member", '2022-9-1' );
+       ("Robby", "Lloyd", "NER", "Member", '2024-8-21'),
+       ("Jackson", "Dumont", "NER", "Member", '2022-9-1' );
 
 -- Create procedure to add a member to the database
+DELIMITER $$
+
+CREATE PROCEDURE AddMemberToClub(
+    IN p_first_name VARCHAR(64),
+    IN p_last_name VARCHAR(64),
+    IN p_join_date DATE,
+    IN p_club_name VARCHAR(64),
+    IN p_role_name VARCHAR(64)
+)
+BEGIN
+    IF EXISTS (SELECT 1 FROM club WHERE club_name = p_club_name) AND
+       EXISTS (SELECT 1 FROM role WHERE role_name = p_role_name) THEN
+
+       INSERT INTO member_of_club (first_name, last_name, join_date, club_name, role_name)
+       VALUES (p_first_name, p_last_name, p_join_date, p_club_name, p_role_name);
+
+ELSE
+SELECT 'Failed to add member: Club or Role does not exist.' AS ErrorMessage;
+END IF;
+END$$
+DELIMITER ;
+
+
+-- Again use a real ID number, 123 is just a placeholder
+CALL RemoveMemberFromClub(123);
 
 -- Create procedure to remove a member from the database
+DELIMITER $$
+CREATE PROCEDURE RemoveMemberFromClub(
+    IN p_member_id INT
+)
+BEGIN
+    IF EXISTS (SELECT 1 FROM member_of_club WHERE member_id = p_member_id) THEN
+DELETE FROM member_of_club WHERE member_id = p_member_id;
+SELECT CONCAT('Member with ID ', p_member_id, ' has been removed successfully.') AS Message;
+
+ELSE
+SELECT CONCAT('Member with ID ', p_member_id, ' does not exist in the database.') AS ErrorMessage;
+END IF;
+END$$
+DELIMITER ;
 
 -- Create procedure to edit a member's role
+DELIMITER $$
+CREATE PROCEDURE EditMemberRole(
+    IN p_member_id INT,
+    IN p_new_role_name VARCHAR(64)
+)
+BEGIN
+    IF EXISTS (SELECT 1 FROM member_of_club WHERE member_id = p_member_id) THEN
+UPDATE member_of_club
+SET role_name = p_new_role_name
+WHERE member_id = p_member_id;
+
+ELSE
+SELECT CONCAT('Member with ID ', p_member_id, ' does not exist in the database.') AS ErrorMessage;
+END IF;
+END$$
+DELIMITER ;
+
+-- use a real Id though 123 is a placeholder, but works!
+-- CALL EditMemberRole(123, 'Treasurer');
 
 -- Create procedure to edit an event in the database
+DELIMITER $$
+CREATE PROCEDURE EditEvent(
+    IN p_event_title VARCHAR(64),
+    IN p_event_date DATE,
+    IN p_club_name VARCHAR(64),
+    IN p_new_event_description VARCHAR(500)
+)
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM event
+        WHERE event_title = p_event_title
+          AND event_date = p_event_date
+          AND club_name = p_club_name
+    ) THEN
+UPDATE event
+SET
+    event_description = p_new_event_description
+WHERE event_title = p_event_title
+  AND event_date = p_event_date
+  AND club_name = p_club_name;
+
+ELSE
+SELECT CONCAT('Event "', p_event_title, '" on "', p_event_date, '" for club "', p_club_name, '" does not exist.') AS ErrorMessage;
+END IF;
+END$$
+DELIMITER ;
+
+CALL EditEvent(
+    'Tech Talk',
+    '2024-12-15',
+    'Generate',
+    'Updated description for the Tech Talk event.'
+);
 
 -- Create procedure to remove an event from the database
+DELIMITER $$
+CREATE PROCEDURE RemoveEvent(
+    IN p_event_title VARCHAR(64),
+    IN p_event_date DATE,
+    IN p_club_name VARCHAR(64)
+)
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM event
+        WHERE event_title = p_event_title
+          AND event_date = p_event_date
+          AND club_name = p_club_name
+    ) THEN
+DELETE FROM event
+WHERE event_title = p_event_title
+  AND event_date = p_event_date
+  AND club_name = p_club_name;
+ELSE
+SELECT CONCAT('Event "', p_event_title, '" on "', p_event_date, '" for club "', p_club_name, '" does not exist.') AS ErrorMessage;
+END IF;
+END$$
+DELIMITER ;
+
+CALL RemoveEvent('Tech Talk', '2024-12-15', 'Generate');
 
 -- Create procedure to add an event to the database
 
+DELIMITER $$
+CREATE PROCEDURE AddEvent(
+    IN p_event_title VARCHAR(64),
+    IN p_event_description VARCHAR(500),
+    IN p_event_date DATE,
+    IN p_club_name VARCHAR(64)
+)
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM club
+        WHERE club_name = p_club_name
+    ) THEN
+        INSERT INTO event (event_title, event_description, event_date, club_name)
+        VALUES (p_event_title, p_event_description, p_event_date, p_club_name);
+ELSE
+SELECT CONCAT('Club "', p_club_name, '" does not exist.') AS ErrorMessage;
+END IF;
+END$$
+DELIMITER ;
+
+CALL AddEvent('Tech Talk', 'An informative event about emerging technologies.', '2024-12-15', 'Generate');
+
 -- Create procedure for determining the number of members in a club
+DELIMITER $$
+CREATE PROCEDURE GetNumberOfMembersInClub(
+    IN p_club_name VARCHAR(64)
+)
+BEGIN
+    -- Count the number of members in the specified club
+SELECT COUNT(*) AS NumberOfMembers
+FROM member_of_club
+WHERE club_name = p_club_name;
+END$$
+DELIMITER ;
+
+CALL GetNumberOfMembersInClub('Generate');
